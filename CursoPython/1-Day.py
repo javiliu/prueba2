ca7443123358b13ccb17da4cfdfd7f0c66779118
Javier Adrián Liu Manzanilla <j4v13rlm@gmail.com>
2016-11-03 10:49:29 -0500

    Agregar dia 1 curso python

---
+++ CursoPython/1-Day.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is the first
+    day of Python
+    course,spanish
+    and english"""
+
+# Comments, Variable, input and Print
+# When you need comment something, just type has character
+# A variable is a memory space where you can save a value
+entero = 12
+flotante = 869.34
+nombre = "Juanelo"
+genero = 'H'
+vivo = True
+
+# If you need print something at the screen, just type print
+print("Variables declaradas son: entero y su valor es ")
+print(entero)
+
+# If you need record the value typed for in a variable just
+# type the variable name, type equal symbol and type input()
+valorUsuario = input("Ingrese un dato")
